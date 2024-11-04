dd38980736cc1387c867911baa735b372927f14a
conor-wilson <conorwilson1998@gmail.com>
2024-11-05 10:58:57 +1100

    Added password text box and its basic functionality

--- start-game/scenes/settings_menu.gd
+++ start-game/scenes/settings_menu.gd
@@ -4,6 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$UsernameBox.hide()
+	$PasswordBox.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,3 +19,12 @@ func _on_secret_settings_button_pressed() -> void:
 func _on_username_box_text_submitted(new_text: String) -> void:
 	print("Username Entered: ", new_text)
 	$UsernameBox.clear()
+	
+	if new_text == "QuietLantern":
+		$PasswordBox.show()
+		$PasswordBox.grab_focus()
+
+
+func _on_password_box_text_submitted(new_text: String) -> void:
+	print("Password Entered: ", new_text)
+	$PasswordBox.clear()
