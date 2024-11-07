90860bcb68f0f6d9bb9c5e7c7c73ac18ab072abb
conor-wilson <conorwilson1998@gmail.com>
2024-11-08 00:08:38 +1100

    Aligned the components of the settings menu a bit better

--- let-me-play/scenes/settings_menu.gd
+++ let-me-play/scenes/settings_menu.gd
@@ -4,26 +4,29 @@ signal back_button
 
 @export var devUsername := "QuietLantern"
 
+@onready var username_box: LineEdit = $CenterContainer/VBoxContainer/UsernameBox
+@onready var password_box: LineEdit = $CenterContainer/VBoxContainer/PasswordBox
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$UsernameBox.hide()
-	$PasswordBox.hide()
+	username_box.hide()
+	password_box.hide()
 
 func _on_secret_settings_button_pressed() -> void:
-	$UsernameBox.show()
-	$UsernameBox.grab_focus()
+	username_box.show()
+	username_box.grab_focus()
 
 func _on_username_box_text_submitted(new_text: String) -> void:
 	print("Username Entered: ", new_text)
-	$UsernameBox.clear()
+	username_box.clear()
 	
 	if new_text == devUsername:
-		$PasswordBox.show()
-		$PasswordBox.grab_focus()
+		password_box.show()
+		password_box.grab_focus()
 
 func _on_password_box_text_submitted(new_text: String) -> void:
 	print("Password Entered: ", new_text)
-	$PasswordBox.clear()
+	password_box.clear()
 
 func _on_button_pressed() -> void:
 	back_button.emit()
