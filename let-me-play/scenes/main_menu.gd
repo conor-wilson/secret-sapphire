e5862cd80ef940ea779672027678d52f1ec86cac
conor-wilson <conorwilson1998@gmail.com>
2024-11-12 01:24:52 +1100

    Initialised poc of initial dialogue sequence in main menu

--- let-me-play/scenes/main_menu.gd
+++ let-me-play/scenes/main_menu.gd
@@ -8,6 +8,14 @@ func _on_start_button_collision() -> void:
 	shake_screen.emit(30.0, 5.0)
 	for letter in $Letters.get_children(): 
 		letter.spawn()
+	
+	# TODO: The below is just a proof of concept. Fix it into something more dynamic and reusable.
+	await get_tree().create_timer(1.5).timeout
+	var lines: Array[String] = [
+		"What was that??",
+		"Is everything ok out there?"
+	]
+	DialogueManager.start_dialogue($SettingsButton.global_position + Vector2(320, 40), lines)
 
 func _on_start_button_click() -> void:
 	shake_screen.emit(10, 5.0)
