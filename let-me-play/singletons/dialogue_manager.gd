d339d1d6ec5ca0ca4e27bc9567560159bf193b37
conor-wilson <conorwilson1998@gmail.com>
2024-11-12 23:05:20 +1100

    Cleaned up dialogue manager

--- let-me-play/singletons/dialogue_manager.gd
+++ let-me-play/singletons/dialogue_manager.gd
@@ -2,53 +2,59 @@ extends Node
 
 @onready var dialogue_box_scene = preload("res://scenes/dialogue_box.tscn")
 
-# TODO: Review this code and tidy it all up
+var dialogue_box # The current instantiation of the dialogue box scene that is being displayed.
+var line_queue: Array[String] = [] # The list of lines that are queued to be displayed.
 
-var dialogue_lines: Array[String] = []
-var current_line_index: int = 0
-
-var dialogue_box
+# TODO: Find a way to get rid of the below vars. They feel unnecessary.
 var dialogue_box_position: Vector2
+var dialogue_linger_time: float = 2
 
-var dialogue_active: bool = false
-var can_advance_line: bool = false
-
-func start_dialogue(position: Vector2, lines: Array[String]): 
-	if dialogue_active:
+# start_dialogue starts a new dialogue session that displays the provided lines
+# at the provided position. The optional linger_time input determines the amount
+# of seconds that each line should stay on the screen before moving on.
+func start_dialogue(position: Vector2, lines: Array[String], linger_time:float=2): 
+	
+	# Exit early if there's already an active dialogue box
+	if dialogue_box != null:
 		return
 	
-	dialogue_lines = lines
+	# Set the class-scoped components
 	dialogue_box_position = position
+	line_queue = lines
+	dialogue_linger_time = linger_time
+	
+	# Start the dialogue
 	_show_dialogue_box()
-	
-	dialogue_active = true
 
-func _show_dialogue_box(): 
+# _show_dialogue_box removes any existing dialogue box, and creates a new one
+# containing the next line in the queue, if it exists.
+func _show_dialogue_box():
+	
+	# Exit early if we're all out of lines in the queue
+	if line_queue.size() == 0:
+		return
+	
+	# Instantiate a new dialogue box
 	dialogue_box = dialogue_box_scene.instantiate()
-	dialogue_box.finished_displaying.connect(_on_dialogue_box_finished_displaying) # TODO: This seems like a race condition
 	get_tree().root.add_child(dialogue_box)
+	
+	# Rig the box up to work as it should
+	dialogue_box.finished_displaying.connect(_on_dialogue_box_finished_displaying)
 	dialogue_box.global_position = dialogue_box_position
-	dialogue_box.display_text(dialogue_lines[current_line_index])
-	can_advance_line = false
+	
+	# Display the next text in the queue
+	dialogue_box.display_text(line_queue.pop_front())
 
+# _on_dialogue_box_finished_displaying waits for the designated 
+# dialogue_linger_time, and then advances the dialogue to the next line (or
+# clears the dialogue box).
 func _on_dialogue_box_finished_displaying():
-	can_advance_line = true
+	await get_tree().create_timer(dialogue_linger_time).timeout
+	_advance_dialogue()
 
-# TODO: Change all the below to be triggered by a timed event rather than via player input.
-func _unhandled_input(event: InputEvent) -> void:
-	if (
-		event.is_action_pressed("advance_dialogue") &&
-		dialogue_active &&
-		can_advance_line
-	): 
+# _advance_dialogue() clears the current dialogue box, and spawns a new one with
+# the next line in the queue if one exists.
+func _advance_dialogue() -> void:
+	if dialogue_box != null: 
 		dialogue_box.queue_free()
-		
-		current_line_index += 1
-		if current_line_index >= dialogue_lines.size():
-			dialogue_active = false
-			current_line_index = 0
-			return
-		
 		_show_dialogue_box()
-	
-	
