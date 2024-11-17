740484f966b1f381983f699c37d4e1743b172d65
conor-wilson <conorwilson1998@gmail.com>
2024-11-18 01:26:21 +1100

    Expanded static-clear circle radius by one square

--- let-me-play/scenes/desktop.gd
+++ let-me-play/scenes/desktop.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal tap
+
 var interactable:bool = false 
 
 ## Called when the node enters the scene tree for the first time.
@@ -15,6 +17,7 @@ func _on_screen_input_event(viewport: Node, event: InputEvent, shape_idx: int) -
 	
 	if event.is_pressed() && event.is_action("click"):
 		_clear_circle(_get_mouse_cell_coords())
+		tap.emit()
 		
 
 func _get_mouse_cell_coords() -> Vector2i:
@@ -25,34 +28,52 @@ func _get_mouse_cell_coords() -> Vector2i:
 	)
 
 const circle_matrix_0:Array[Vector2i] = [
+									  Vector2i(-1,-3), 				   Vector2i(1,-3),
+					  				   				   				   				   				  
+	Vector2i(-3,-1),  				   				   				   				   				   Vector2i(3,-1), 
+	 				   				   				   				   				   				   				  
+	Vector2i(-3, 1),  				   				   				   				   				   Vector2i(3, 1),
+					  				   				   				   				   				  
+									  Vector2i(-1, 3),  			   Vector2i(1, 3),
+]
+const circle_matrix_1:Array[Vector2i] = [
+									  				  Vector2i(0,-3), 				 
+					 Vector2i(-2,-2), 												   Vector2i(2,-2),
+					 				  												   				   				  
+	Vector2i(-3, 0), 																				   Vector2i(3, 0),
+					 																				   				  
+					 Vector2i(-2, 2), 												   Vector2i(2, 2),
+									  				  Vector2i(0, 3), 				  
+]
+const circle_matrix_2:Array[Vector2i] = [
 					 Vector2i(-1,-2),                 Vector2i(1,-2),
 	Vector2i(-2,-1),                                                  Vector2i(2,-1),
 																					 
 	Vector2i(-2, 1),                                                  Vector2i(2, 1),
 					 Vector2i(-1, 2),                 Vector2i(1, 2),
 ]
-const circle_matrix_1:Array[Vector2i] = [
+const circle_matrix_3:Array[Vector2i] = [
 									  Vector2i(0,-2),                 
 																					 
 	Vector2i(-2, 0),                                                  Vector2i(2, 0),
 																					 
 									  Vector2i(0, 2),                 
 ]
-const circle_matrix_2:Array[Vector2i] = [
+const circle_matrix_4:Array[Vector2i] = [
 																	 
 					 Vector2i(-1,-1),                 Vector2i(1,-1),                
 																					 
 					 Vector2i(-1, 1),                 Vector2i(1, 1),                
 																	 
 ]
-const circle_matrix_3:Array[Vector2i] = [
+const circle_matrix_5:Array[Vector2i] = [
 																	 
 									  Vector2i(0,-1),                                
 					 Vector2i(-1, 0),                 Vector2i(1, 0),                
 									  Vector2i(0, 1),                                
 																	 
 ]
-const circle_matrix_4:Array[Vector2i] = [
+const circle_matrix_6:Array[Vector2i] = [
 																	 
 																					 
 									  Vector2i(0, 0),                                
@@ -60,11 +81,13 @@ const circle_matrix_4:Array[Vector2i] = [
 																	 
 ]
 const circle_matrix_full:Array[Vector2i] = [
-					 Vector2i(-1,-2), Vector2i(0,-2), Vector2i(1,-2),
-	Vector2i(-2,-1), Vector2i(-1,-1), Vector2i(0,-1), Vector2i(1,-1), Vector2i(2,-1),
-	Vector2i(-2, 0), Vector2i(-1, 0), Vector2i(0, 0), Vector2i(1, 0), Vector2i(2, 0),
-	Vector2i(-2, 1), Vector2i(-1, 1), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1),
-					 Vector2i(-1, 2), Vector2i(0, 2), Vector2i(1, 2),
+									  Vector2i(-1,-3), Vector2i(0,-3), Vector2i(1,-3),
+					 Vector2i(-2,-2), Vector2i(-1,-2), Vector2i(0,-2), Vector2i(1,-2), Vector2i(2,-2),
+	Vector2i(-3,-1), Vector2i(-2,-1), Vector2i(-1,-1), Vector2i(0,-1), Vector2i(1,-1), Vector2i(2,-1), Vector2i(3,-1), 
+	Vector2i(-3, 0), Vector2i(-2, 0), Vector2i(-1, 0), Vector2i(0, 0), Vector2i(1, 0), Vector2i(2, 0), Vector2i(3, 0),
+	Vector2i(-3, 1), Vector2i(-2, 1), Vector2i(-1, 1), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(3, 1),
+					 Vector2i(-2, 2), Vector2i(-1, 2), Vector2i(0, 2), Vector2i(1, 2), Vector2i(2, 2),
+									  Vector2i(-1, 3), Vector2i(0, 3), Vector2i(1, 3),
 ]
 
 # _clear_circle clears a circle of static centred at the provided origin point, 
@@ -75,6 +98,12 @@ const circle_matrix_full:Array[Vector2i] = [
 func _clear_circle(origin:Vector2i):
 	
 	# Clear static in a circle
+	for coords in circle_matrix_6:
+		$Static.erase_cell(coords+origin)
+	#await get_tree().create_timer(0.02).timeout
+	for coords in circle_matrix_5:
+		$Static.erase_cell(coords+origin)
+	await get_tree().create_timer(0.02).timeout
 	for coords in circle_matrix_4:
 		$Static.erase_cell(coords+origin)
 	#await get_tree().create_timer(0.02).timeout
@@ -105,5 +134,11 @@ func _clear_circle(origin:Vector2i):
 		$Static.set_cell(coords+origin, 1, Vector2i(0,randi_range(0,1)), randi_range(1,8))
 	await get_tree().create_timer(0.02).timeout
 	for coords in circle_matrix_4:
+		$Static.set_cell(coords+origin, 1, Vector2i(0,randi_range(0,1)), randi_range(1,8))
+	await get_tree().create_timer(0.02).timeout
+	for coords in circle_matrix_5:
+		$Static.set_cell(coords+origin, 1, Vector2i(0,randi_range(0,1)), randi_range(1,8))
+	await get_tree().create_timer(0.02).timeout
+	for coords in circle_matrix_6:
 		$Static.set_cell(coords+origin, 1, Vector2i(0,randi_range(0,1)), randi_range(1,8))
 	await get_tree().create_timer(0.02).timeout
