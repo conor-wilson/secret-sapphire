0e753125ee76252773ca1a24c2627bed8dbcad89
conor-wilson <conorwilson1998@gmail.com>
2024-11-17 22:51:58 +1100

    Refactored desktop to clear a circle of static around the clicked location

--- let-me-play/scenes/desktop.gd
+++ let-me-play/scenes/desktop.gd
@@ -4,7 +4,7 @@ var interactable:bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	interactable = false
+	interactable = true
 
 
 func _on_screen_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
@@ -14,8 +14,8 @@ func _on_screen_input_event(viewport: Node, event: InputEvent, shape_idx: int) -
 		return
 	
 	if event.is_pressed() && event.is_action("click"):
-		var coords = _get_mouse_cell_coords()
-		$Static.erase_cell(coords)
+		_clear_circle(_get_mouse_cell_coords())
+		
 
 func _get_mouse_cell_coords() -> Vector2i:
 	var coords:Vector2 = get_global_mouse_position() 
@@ -23,3 +23,16 @@ func _get_mouse_cell_coords() -> Vector2i:
 		floor(coords.x/32),
 		floor(coords.y/32),
 	)
+
+const circle_matrix:Array[Vector2i] = [
+						 Vector2i(-1,-2), Vector2i(0,-2), Vector2i(1,-2),
+		Vector2i(-2,-1), Vector2i(-1,-1), Vector2i(0,-1), Vector2i(1,-1), Vector2i(2,-1),
+		Vector2i(-2, 0), Vector2i(-1, 0), Vector2i(0, 0), Vector2i(1, 0), Vector2i(2, 0),
+		Vector2i(-2, 1), Vector2i(-1, 1), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1),
+						 Vector2i(-1, 2), Vector2i(0, 2), Vector2i(1, 2),
+	]
+
+func _clear_circle(origin:Vector2i):
+	for coords in circle_matrix:
+		$Static.erase_cell(coords+ origin)
+	
