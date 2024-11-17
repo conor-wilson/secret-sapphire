ef3630b11caa5e75a5c80a96862b7fef0c9fa18d
conor-wilson <conorwilson1998@gmail.com>
2024-11-18 01:10:24 +1100

    Added desktop background placeholder

--- let-me-play/scenes/desktop.gd
+++ let-me-play/scenes/desktop.gd
@@ -2,9 +2,9 @@ extends Node2D
 
 var interactable:bool = false 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	interactable = true
+## Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	#interactable = true
 
 
 func _on_screen_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
