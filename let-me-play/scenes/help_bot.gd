5647d581717d743051e7fa31b0efdbcdfe2b3f92
conor-wilson <conorwilson1998@gmail.com>
2024-11-18 18:56:51 +1100

    Added idle floating animation to HelpBot

---
+++ let-me-play/scenes/help_bot.gd
@@ -0,0 +1,32 @@
+extends Area2D
+
+@export var speed:float = 25
+@export var target_markers:Array[Marker2D]
+
+var target:Marker2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	if target_markers.size() == 0:
+		
+		var default_marker = Marker2D.new()
+		add_child(default_marker)
+		
+		target_markers = [default_marker]
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	set_idle_movement_target()
+	move(delta)
+
+func set_idle_movement_target():
+	if target == null || position.distance_to(target.position) <10:
+		target = target_markers[randi_range(0, target_markers.size()-1)]
+
+func move(delta:float):
+	
+	var direction:Vector2 = (target.position - position).normalized()
+	
+	position += direction * speed * delta
