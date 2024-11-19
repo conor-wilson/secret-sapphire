0a0d88bbc6e1d47d36a0f933f7b606de0144f53a
conor-wilson <conorwilson1998@gmail.com>
2024-11-19 19:08:28 +1100

    Added random blinking to help bot

--- let-me-play/scenes/help_bot.gd
+++ let-me-play/scenes/help_bot.gd
@@ -3,10 +3,14 @@ extends Area2D
 @export var speed:float = 25
 @export var target_markers:Array[Marker2D]
 
+
 var target:Marker2D
+@onready var blink_length:float = $Sprite2D/AnimationPlayer.get_animation("help_bot_idle").length
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
+	
 	
 	if target_markers.size() == 0:
 		
@@ -30,3 +34,16 @@ func move(delta:float):
 	var direction:Vector2 = (target.position - position).normalized()
 	
 	position += direction * speed * delta
+#
+## TODO: This is purely for debugging. This function should be removed once it's
+## no-longer needed.
+#func _input(event: InputEvent) -> void:
+	#if event.is_action_pressed("debugbutton"): 
+		#_animate_blink()
+
+
+func _on_blink_timer_timeout() -> void:
+	$Sprite2D/AnimationPlayer.play("help_bot_idle")
+	await get_tree().create_timer(blink_length).timeout
+	$Sprite2D/AnimationPlayer.play("RESET")
+	$BlinkTimer.start(randf_range(0.0, 5.0))
