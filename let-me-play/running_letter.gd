942c1dff0d1f4f87e38dac93c463add44b0e8298
conor-wilson <conorwilson1998@gmail.com>
2024-11-22 22:55:31 +1100

    Fixed up letter jump animation

--- let-me-play/running_letter.gd
+++ let-me-play/running_letter.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -600.0
 
 @export var direction:Vector2 = Vector2.RIGHT
 var activated:bool=false
@@ -25,7 +25,9 @@ func _physics_process(delta: float) -> void:
 	#var direction := Input.get_axis("ui_left", "ui_right")
 	#if direction:
 	if activated:
+		_set_animation()
 		velocity.x = direction.x * SPEED
+		_check_apex()
 	#else:
 		#velocity.x = move_toward(velocity.x, 0, SPEED)
 
@@ -37,13 +39,11 @@ func activate():
 		$ChangeDirTimer.start(randf_range(0.1, 1))
 		$JumpTimer.start(randf_range(0.1, 1))
 		activated = true
-		_set_animation()
 
 func _on_change_dir_timer_timeout() -> void:
 	if is_on_floor():
 		direction = direction*(-1)
 	$ChangeDirTimer.start(randf_range(0.1, 1))
-	_set_animation()
 
 func _on_jump_timer_timeout() -> void:
 	if is_on_floor():
@@ -52,7 +52,25 @@ func _on_jump_timer_timeout() -> void:
 
 func _set_animation():
 	if direction == Vector2.RIGHT:
+		if !is_on_floor():
+			$AnimatedSprite2D.play("s-jumping_right")
 		$AnimatedSprite2D.play("s-running_right")
 	else:
+		if !is_on_floor():
+			$AnimatedSprite2D.play("s-jumping_left")
 		$AnimatedSprite2D.play("s-running_left")
 	
+
+
+func _on_left_wall_detector_body_entered(body: Node2D) -> void:
+	direction = Vector2.RIGHT
+
+func _on_right_wall_detector_body_entered(body: Node2D) -> void:
+	direction = Vector2.LEFT
+
+
+func _check_apex():
+	if !is_on_floor() && velocity.y >= -0.01:
+		
+		print("YO")
+		z_index = -100
