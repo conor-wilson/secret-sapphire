a1e3b1fcfac0a467a9dab9dad8a58dd72525ba31
conor-wilson <conorwilson1998@gmail.com>
2024-11-23 12:01:14 +1100

    Added jump animation

--- let-me-play/running_letter.gd
+++ let-me-play/running_letter.gd
@@ -5,8 +5,11 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
 
 @export var direction:Vector2 = Vector2.RIGHT
-var activated:bool=false
 var is_infront_of_desktop:bool = false
+var ready_to_exit:bool = false
+
+enum State {IDLE, RUNNING, JUMPING, LANDING}
+var state:State = State.IDLE
 
 #func _ready() -> void:
 	#$AnimatedSprite2D.play("s-running")
@@ -16,30 +19,25 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		activate()
-		#velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	#var direction := Input.get_axis("ui_left", "ui_right")
 	#if direction:
-	if activated:
-		_set_animation()
-		velocity.x = direction.x * SPEED
-		_enter_desktop_if_able()
+	
+	_set_velocity()
+	move_and_slide()
+	_enter_desktop_if_able()
+	_set_animation()
+	
 	#else:
 		#velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
 
-func activate(): 
-	
-	if !activated:
+func start_running(): 
+	if state != State.RUNNING:
 		$ChangeDirTimer.start(randf_range(0.1, 1))
 		$JumpTimer.start(randf_range(0.1, 1))
-		activated = true
+		state = State.RUNNING
 
 func _on_change_dir_timer_timeout() -> void:
 	if is_on_floor():
@@ -47,19 +45,37 @@ func _on_change_dir_timer_timeout() -> void:
 	$ChangeDirTimer.start(randf_range(0.1, 1))
 
 func _on_jump_timer_timeout() -> void:
+	$JumpTimer.start(randf_range(1.1, 3))
 	if is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	$JumpTimer.start(randf_range(0.1, 3))
+		_jump()
+
+func _jump():
+	state = State.JUMPING
+	await $AnimatedSprite2D.frame_changed
+	await $AnimatedSprite2D.frame_changed
+	await $AnimatedSprite2D.frame_changed
+	velocity.y = JUMP_VELOCITY
+	
 
 func _set_animation():
-	if direction == Vector2.RIGHT:
-		if !is_on_floor():
-			$AnimatedSprite2D.play("s-jumping_right")
-		$AnimatedSprite2D.play("s-running_right")
+	
+	if state == State.JUMPING:
+		if $AnimatedSprite2D.animation != "s-jumping":
+			print($AnimatedSprite2D.animation)
+			$AnimatedSprite2D.play("s-jumping")
+	elif state == State.LANDING:
+		$AnimatedSprite2D.play_backwards("s-jumping")
+	elif state == State.RUNNING:
+		if direction == Vector2.RIGHT:
+			$AnimatedSprite2D.play("s-running_right")
+		else:
+			$AnimatedSprite2D.play("s-running_left")
 	else:
-		if !is_on_floor():
-			$AnimatedSprite2D.play("s-jumping_left")
-		$AnimatedSprite2D.play("s-running_left")
+		$AnimatedSprite2D.play("s-idle")
+
+func _set_velocity():
+	if state == State.RUNNING || state == State.JUMPING || state == State.LANDING:
+		velocity.x = direction.x * SPEED
 
 func _on_left_wall_detector_body_entered(body: Node2D) -> void:
 	direction = Vector2.RIGHT
@@ -75,9 +91,33 @@ func _on_right_wall_detector_body_entered(body: Node2D) -> void:
 		#z_index = -100
 
 func _enter_desktop_if_able():
-	if !is_on_floor() && velocity.y >= -0.01 && is_infront_of_desktop:
+	if !is_on_floor() && velocity.y >= -0.01 && is_infront_of_desktop && ready_to_exit:
 		z_index = -1
 
 func set_can_enter_desktop(val:bool):
-	#print("ASDF")
 	is_infront_of_desktop = val
+
+# TODO: This is purely for debugging. This function should be removed once it's
+# no-longer needed.
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("debugbutton"):
+		$MischiefTimer.start()
+		start_running()
+
+
+func _on_landing_detector_body_entered(body: Node2D) -> void:
+	
+	# TODO: This feels a little bit weird. Rethink it.
+	if z_index == -1:
+		queue_free()
+	
+	if state == State.JUMPING:
+		state = State.LANDING
+		await $AnimatedSprite2D.frame_changed
+		await $AnimatedSprite2D.frame_changed
+		await $AnimatedSprite2D.frame_changed
+		start_running()
+
+
+func _on_mischief_timer_timeout() -> void:
+	ready_to_exit = true
