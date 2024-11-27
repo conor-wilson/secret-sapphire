134eb7db1636f9d29e6337d529b21f4f2a5d4596
conor-wilson <conorwilson1998@gmail.com>
2024-11-27 11:16:01 +1100

    Refactored fire to be able to be put out by FireExtinguisher

--- let-me-play/scenes/fire.gd
+++ let-me-play/scenes/fire.gd
@@ -1,4 +1,4 @@
-extends AnimatedSprite2D
+extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -6,13 +6,23 @@ func _ready() -> void:
 	await get_tree().create_timer(randf_range(0, 0.4)).timeout
 	
 	match randi_range(0,2):
-		0: play("fire_0")
-		1: play("fire_1")
-		2: play("fire_2")
+		0: $AnimatedSprite2D.play("fire_0")
+		1: $AnimatedSprite2D.play("fire_1")
+		2: $AnimatedSprite2D.play("fire_2")
 	
 	if randi_range(0,1) == 1:
-		flip_h = true
+		$AnimatedSprite2D.flip_h = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event.is_action_pressed("click") && CursorManager.current_cursor == CursorManager.FIRE_EXTINGUISHER:
+		hide()
+
+
+func _on_mouse_entered() -> void:
+	if Input.is_action_pressed("click") && CursorManager.current_cursor == CursorManager.FIRE_EXTINGUISHER:
+		hide()
