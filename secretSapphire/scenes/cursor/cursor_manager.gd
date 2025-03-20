087c548c95f4adcd47ff08a9de3241336cf3a3a6
conor-wilson <conorwilson1998@gmail.com>
2025-03-20 21:24:04 +1100

    Fixed cursors to have custom offsets so it feels more natural

--- secretSapphire/scenes/cursor/cursor_manager.gd
+++ secretSapphire/scenes/cursor/cursor_manager.gd
@@ -8,8 +8,23 @@ extends Node
 var current_cursor:Resource
 
 func set_mouse_cursor(source:Resource):
-	Input.set_custom_mouse_cursor(source)
+	Input.set_custom_mouse_cursor(source, 0, _get_hotspot(source))
 	current_cursor = source
+	
 
 func current_cursor_is(source:Resource) -> bool:
 	return current_cursor == source
+
+# _get_hotspot returns the hotspot offset for the provided custom cursor resource.
+func _get_hotspot(source:Resource) -> Vector2: 
+	match source:
+		CURSOR:
+			return Vector2.ZERO
+		WRENCH:
+			return Vector2(4, 4)
+		FIRE_EXTINGUISHER: 
+			return Vector2(0, 4)
+		CRUMPLED_PAPER:
+			return Vector2(64, 64)
+		_:
+			return Vector2.ZERO
