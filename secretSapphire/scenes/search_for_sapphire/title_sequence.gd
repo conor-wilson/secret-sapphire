1ec0b73e673e442b2db77c97c424fcec3d262d85
conor-wilson <conorwilson1998@gmail.com>
2025-10-27 17:52:15 +1100

    Smoothed out the whole eyes background sequence

--- secretSapphire/scenes/search_for_sapphire/title_sequence.gd
+++ secretSapphire/scenes/search_for_sapphire/title_sequence.gd
@@ -2,6 +2,7 @@ class_name TitleSequence extends Node2D
 
 signal done
 
+@onready var eyes_background: AnimatedSprite2D = $EyesBackground
 @onready var photosensitivity_screen: ColorRect = $PhotosensitivityScreen
 @onready var credit_screen: ColorRect = $CreditScreen
 @onready var title_screen: ColorRect = $TitleScreen
@@ -11,27 +12,55 @@ signal done
 
 func play_title_sequence(previous_screen:Node, final_screen:Node, linger_time:float = 3.0, hard_cut:bool=false, include_music:bool = false, include_photo_warning:bool=false):
 	
+	# Null safety
 	if previous_screen == null:
 		previous_screen = black_screen
 	
+	# Play the intro track if required
 	if include_music && !Global.music_muted:
 		spooky_intro_music.play()
 	
-	if hard_cut:
-		previous_screen.hide()
-		cut_to_black()
-		previous_screen = black_screen
+	# Only do a smooth fade if not hard-cutting
+	if !hard_cut:
+		TransitionFade.transition()
+		await TransitionFade.fully_black
+	previous_screen.hide()
 	
+	# Begin the background animation
+	eyes_background.play("intro")
+	eyes_background.show()
+	
+	# Execute the sequence
 	if include_photo_warning:
-		await transition_screen(previous_screen, photosensitivity_screen, linger_time)
-		await transition_screen(photosensitivity_screen, credit_screen, linger_time)
-	else:
-		await transition_screen(previous_screen, credit_screen, linger_time)
+		await fade_in(photosensitivity_screen, linger_time)
+	await fade_in(credit_screen, linger_time)
+	await fade_in(title_screen, linger_time, false)
 	
-	await transition_screen(credit_screen, title_screen, linger_time)
-	done.emit()
+	# Transition to the next scene
+	TransitionFade.transition()
+	await TransitionFade.fully_black
+	eyes_background.hide()
+	title_screen.hide()
+	final_screen.show()
 	spooky_intro_music.stop()
-	transition_screen(title_screen, final_screen, linger_time)
+	done.emit()
+
+
+func fade_in(screen:ColorRect, linger_time:float, also_fade_out:bool = true):
+	
+	# Find the animation player
+	var animation_player:AnimationPlayer = screen.find_child("AnimationPlayer")
+	
+	# Fade the screen in
+	screen.show()
+	animation_player.play("fade_in")
+	await get_tree().create_timer(linger_time).timeout
+	
+	# Optionally fade the screen out
+	if also_fade_out:
+		animation_player.play("fade_out")
+		await animation_player.animation_finished
+		screen.hide()
 
 
 func transition_screen(from_screen:Node, to_screen:Node, linger_time:float = 3.0):
@@ -46,3 +75,13 @@ func cut_to_black():
 	credit_screen.hide()
 	title_screen.hide()
 	black_screen.show()
+
+
+func _on_black_screen_visibility_changed() -> void:
+	if black_screen != null && black_screen.visible:
+		$EyesBackground.play("intro")
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if $EyesBackground.animation == "intro":
+		$EyesBackground.play("loop")
